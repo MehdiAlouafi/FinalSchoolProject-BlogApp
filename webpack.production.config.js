731003645110471d3bef36b1630137d7480f3954
Mehdi Al Ouafi <alouafi.mehdi@gmail.com>
2016-08-28 23:43:38 +0200

    please

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -48,7 +48,7 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel',
       query: {
-        "presets": ["react", "es2015", "stage-0", "react-hmre"]
+        "presets": ["react", "es2015", "stage-0"]
       }
     }, {
       test: /\.json?$/,
